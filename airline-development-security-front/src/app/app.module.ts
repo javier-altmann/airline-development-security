9ab1065fd28bee20b5de11f5aed2df8c427760cd
javier-altmann <javier.altmann@hotmail.com>
2017-10-21 17:24:57 -0300

    Elimino pantallas principales
    Elimino todaas las pantallas principales por ahora no se van a usar.

--- airline-development-security-front/src/app/app.module.ts
+++ airline-development-security-front/src/app/app.module.ts
@@ -10,13 +10,12 @@ import { MenuAnalistaComponent } from './Components/shared/menu-analista/menu-an
 import { MenuVendedorComponent } from './Components/shared/menu-vendedor/menu-vendedor.component';
 import { AltaAvionesComponent } from './Components/globales/alta-aviones/alta-aviones.component';
 import { AltaDestinosComponent } from './Components/globales/alta-destinos/alta-destinos.component';
-import { PaginaPrincipalGlobalesComponent } from './Components/globales/pagina-principal-globales/pagina-principal-globales.component';
-import { PaginaPrincipalAnalistaComponent } from './Components/analista/pagina-principal-analista/pagina-principal-analista.component';
 import { ProgramarVuelosComponent } from './Components/analista/programar-vuelos/programar-vuelos.component';
 import { AnalisisGananciasComponent } from './Components/analista/analisis-ganancias/analisis-ganancias.component';
 import { VentaTicketsComponent } from './Components/vendedor/venta-tickets/venta-tickets.component';
 import { CerrarVuelosComponent } from './Components/vendedor/cerrar-vuelos/cerrar-vuelos.component';
 import { MenuGlobalComponent } from './Components/shared/menu-global/menu-global.component';
+
 
 
 @NgModule({
@@ -28,8 +27,6 @@ import { MenuGlobalComponent } from './Components/shared/menu-global/menu-global
     MenuVendedorComponent,
     AltaAvionesComponent,
     AltaDestinosComponent,
-    PaginaPrincipalGlobalesComponent,
-    PaginaPrincipalAnalistaComponent,
     ProgramarVuelosComponent,
     AnalisisGananciasComponent,
     VentaTicketsComponent,
