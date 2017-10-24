3bb724ae61052e0a139595bade4fdf85840b007a
javier-altmann <javier.altmann@hotmail.com>
2017-10-23 22:55:30 -0300

    Subo proyecto preparado para Deploy
    Subo proyecto preparado con todas las configuraciones para hacer el
    Deploy.

--- airline-development-security-front/e2e/app.e2e-spec.ts
+++ airline-development-security-front/e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { AirlineDevelopmentSecurityPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('airline-development-security App', () => {
-  let page: AirlineDevelopmentSecurityPage;
+describe('proyecto App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new AirlineDevelopmentSecurityPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
